2bde44361d20268520ae66174d31a97c6e139bdc
Zheng <zhoupt@bc.edu>
2020-11-01 02:31:38 -0500

    DPlayer front end

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### References
+* [Dplayer](http://dplayer.js.org/guide.html#installation)
+* [Backend](https://github.com/MoePlayer/DPlayer-node)
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import publicUrl from '../utils/publicUrl';
 import Navbar from './Navbar';
+import DPlayer from 'dplayer';
 import css from './Home.module.css';
 
 function Home() {
+  const options = {
+    container: document.getElementById('dplayer'),
+    screenshot: true,
+    video: {
+       url: publicUrl('/assets/Firstclass.mp4'),
+       pic: publicUrl('/assets/bclogo.jpeg'),
+    },
+    danmaku: {
+      id:'Firstclass',
+      api: 'https://api.prprpr.me/dplayer/',
+      addition: ['https://api.prprpr.me/dplayer/v3/bilibili?aid=[aid]'],
+    },
+};
+  const dp = new DPlayer(options);
+  dp.danmaku.send(
+    {
+        text: 'dplayer is amazing',
+        color: '#b7daff',
+        type: 'right', // should be `top` `bottom` or `right`
+    },
+    function () {
+        console.log('success');
+    }
+);
+dp.danmaku.draw({
+  text: 'DIYgod is amazing',
+  color: '#fff',
+  type: 'top',
+});
+dp.danmaku.show();
   return (
     <div>
       <div>
       <h2>CSCI3359 Interactive Classroom</h2>
       <br></br>
-      <video width="1000" height="1000" controls src={publicUrl('/assets/Firstclass.mp4')}></video> 
+
     </div>
     <div>
       <Navbar/>
